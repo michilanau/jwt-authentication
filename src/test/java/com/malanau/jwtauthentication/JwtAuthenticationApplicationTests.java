1f58ed444ddd714fdd02d73cc50b6d8a9c4e886c
malanau <miglan99@gmail.com>
2023-10-29 23:06:53 +0100

    Initial commit

---
+++ src/test/java/com/malanau/jwtauthentication/JwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malanau.jwtauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
